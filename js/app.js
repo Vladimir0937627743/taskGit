0686e7447376857eeb7bbdd9a773adc9b1d8ce2b
Vladimir <sobranie.ua@gmail.com>
2017-08-15 14:20:29 +0300

    Feature: add js file

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//var li = document.querySelectorAll("li");
+//var div = document.querySelectorAll(".characteristic");
+//for (var i = 0; i < li.length; i++) {
+ //   (function(a){
+ //       a.onclick = function() {
+//            a.classList.add("active");
+//        }
+//
+//    }(li[i]));
+//}
+//changesfor (var j = 0; j < li.length; j++) {
+//li[j].classList.remove("active");
+//}
+//a.classList.add("active");
+//}
+//for (var d = 0; d < div.length; d++) {
+//if (a != dic[d]) {
+//    div[d].classList.remove("active");
+//} else {
+  //  div[d].classList.add("active");
+//}
+//}
+var li = document.querySelectorAll("li");
+li.onclick = function() {
+   alert("hello");
+};
